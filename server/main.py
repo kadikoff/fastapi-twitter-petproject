204ee8670542743669af4c7780a1eae3d0866d68
Artyom Kadikov <8961805@gmail.com>
2025-04-21 12:40:50 +0300

    Update pre-commit; add hooks

---
+++ server/main.py
@@ -0,0 +1,20 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+PORT: int = 8000
+
+
+@app.get("/api/hello/")
+def hello_index():
+    return {"message": "Hello World!"}
+
+
+if __name__ == "__main__":
+
+    uvicorn.run(
+        host="127.0.0.1",
+        port=PORT,
+        app="main:app",
+        reload=True,
+    )
