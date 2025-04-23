97172c238f38133cd16ebb095003c7a2cec416f0
Artyom Kadikov <8961805@gmail.com>
2025-04-23 12:13:25 +0300

    Create database config for get url

---
+++ server/core/config.py
@@ -0,0 +1,34 @@
+import os
+from pathlib import Path
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+BASE_DIR = Path(__file__).parent.parent.parent
+
+
+class DbSettings(BaseSettings):
+    db_user: str
+    db_pass: str
+    db_host: str
+    db_port: int
+    db_name: str
+    db_echo: bool = True
+
+    @property
+    def url(self):
+        return (
+            f"postgresql+asyncpg"
+            f"://{self.db_user}:{self.db_pass}@{self.db_host}"
+            f":{self.db_port}/{self.db_name}"
+        )
+
+    model_config = SettingsConfigDict(
+        env_file=os.path.join(BASE_DIR, ".env"), env_file_encoding="utf-8"
+    )
+
+
+class Settings(BaseSettings):
+    db: DbSettings = DbSettings()
+
+
+settings = Settings()
