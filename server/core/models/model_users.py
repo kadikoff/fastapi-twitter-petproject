b35bd221cb585519d15122cc0c5013900027a5d3
artiom_kadikov <mister-kadikov@yandex.ru>
2025-04-30 16:31:12 +0300

    Create association table 'followers'

--- server/core/models/model_users.py
+++ server/core/models/model_users.py
@@ -1,6 +1,6 @@
 from typing import TYPE_CHECKING
 
-from sqlalchemy import String
+from sqlalchemy import String, Table, Column, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .model_base import Base
@@ -8,6 +8,14 @@ from .model_base import Base
 if TYPE_CHECKING:
     from .model_likes import Likes
     from .model_tweets import Tweets
+
+
+followers_association_table = Table(
+    "followers_association",
+    Base.metadata,
+    Column("follower_id", ForeignKey("users.id"), primary_key=True),
+    Column("followed_id", ForeignKey("users.id"), primary_key=True),
+)
 
 
 class Users(Base):
@@ -21,6 +29,22 @@ class Users(Base):
         "Tweets", back_populates="user", cascade="all, delete-orphan"
     )
     likes: Mapped[list["Likes"]] = relationship("Likes", back_populates="user")
+
+    followers: Mapped[list["Users"]] = relationship(
+        "Users",
+        secondary=followers_association_table,
+        primaryjoin=(followers_association_table.c.followed_id == id),
+        secondaryjoin=(followers_association_table.c.follower_id == id),
+        back_populates="followed",
+    )
+    followed: Mapped[list["Users"]] = relationship(
+        "Users",
+        secondary=followers_association_table,
+        primaryjoin=(followers_association_table.c.follower_id == id),
+        secondaryjoin=(followers_association_table.c.followed_id == id),
+        back_populates="follower",
+        lazy="selectin",
+    )
 
     def __repr__(self):
         return f"User: id={self.id}, name={self.name}, api_key={self.api_key}"
