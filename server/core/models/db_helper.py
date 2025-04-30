9270a808793aaf71aaab9cc9deeedf08db33fef7
artiom_kadikov <mister-kadikov@yandex.ru>
2025-04-30 03:53:05 +0300

    Pre-commit edit

--- server/core/models/db_helper.py
+++ server/core/models/db_helper.py
@@ -1,4 +1,10 @@
-from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from typing import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
 
 from server.core.config import settings
 
@@ -15,6 +21,11 @@ class DatabaseHelper:
             autocommit=False,
             expire_on_commit=False,
         )
+
+    async def session_dependency(self) -> AsyncGenerator[AsyncSession, None]:
+        async with self.session_factory() as session:
+            yield session
+            await session.close()
 
 
 db_helper = DatabaseHelper(
