3bfdb76b103ce874caff06434ebf3c732681b769
artiom_kadikov <mister-kadikov@yandex.ru>
2025-04-30 03:44:58 +0300

    Init alembic, create migrations

--- alembic/env.py
+++ alembic/env.py
@@ -6,6 +6,8 @@ from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
+from server.core.config import settings
+from server.core.models import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -20,8 +22,6 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-from server.core.models import Base
-from server.core.config import settings
 
 target_metadata = Base.metadata
 config.set_main_option("sqlalchemy.url", settings.db.url_local)
