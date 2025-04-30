25cf9c3951ac0fa20f0339b859ddf6d70857a627
artiom_kadikov <mister-kadikov@yandex.ru>
2025-04-30 04:10:27 +0300

    Some changes

--- server/main.py
+++ server/main.py
@@ -1,38 +1,49 @@
 from contextlib import asynccontextmanager
+from typing import Any, AsyncGenerator
 
 import uvicorn
 from fastapi import FastAPI
 
-from server.api.routers import router
-from server.core.models import Base, db_helper
+from server.api.routes import router
+from server.core.config import BASE_MEDIAS_DIR
+from server.core.models import Users, db_helper
+from server.error_handlers import register_errors_handlers
 
 PORT: int = 8000
 
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
+async def create_mock_data() -> None:
+    user_1 = Users(name="Artyom Kadikov", api_key="test")
+    user_2 = Users(name="Nikita Ivanov", api_key="dev")
 
-    async with db_helper.engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
-        await conn.run_sync(Base.metadata.create_all)
+    async with db_helper.session_factory() as session:
+        session.add_all([user_1, user_2])
+        await session.commit()
+        await session.close()
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> AsyncGenerator[None, Any]:
+
+    BASE_MEDIAS_DIR.mkdir(parents=True, exist_ok=True)
+
+    # await create_mock_data()
 
     yield
+
+    # await db_helper.engine.dispose()
 
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(router=router)
-
-
-@app.get("/api/hello/")
-def hello_index():
-    return {"message": "Hello World!"}
+register_errors_handlers(app=app)
 
 
 if __name__ == "__main__":
 
     uvicorn.run(
-        host="127.0.0.1",
-        port=PORT,
+        host="0.0.0.0",
+        port=8000,
         app="main:app",
         reload=True,
     )
