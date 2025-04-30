0a809ede0537a6028cf5be62ad6d4ecd285d32af
artiom_kadikov <mister-kadikov@yandex.ru>
2025-04-30 16:45:45 +0300

    Add followers_association_table

--- server/core/models/__init__.py
+++ server/core/models/__init__.py
@@ -6,11 +6,12 @@ __all__ = (
     "Tweets",
     "Likes",
     "Medias",
+    "followers_association_table",
 )
 
 from .model_base import Base
 from .db_helper import DatabaseHelper, db_helper
-from .model_users import Users
+from .model_users import Users, followers_association_table
 from .model_tweets import Tweets
 from .model_likes import Likes
 from .model_medias import Medias
