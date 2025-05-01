48142bae87b119d2d9b65d933e08e24da30f1cc9
artiom_kadikov <mister-kadikov@yandex.ru>
2025-05-01 19:24:26 +0300

    Some changes

--- tests/testing_unit/utils/test_media_writer.py
+++ tests/testing_unit/utils/test_media_writer.py
@@ -10,29 +10,28 @@ from server.utils.media_writer import (
 
 
 @pytest.mark.asyncio
-async def test_create_medias_directory_success(medias_folder_path):
-    assert medias_folder_path.exists() == False
-    await create_medias_directory(path=medias_folder_path)
-    assert medias_folder_path.exists()
+async def test_create_medias_directory_success(path_to_medias_dir):
+    await create_medias_directory(path=path_to_medias_dir)
+    assert path_to_medias_dir.exists()
 
 
 @pytest.mark.asyncio
-async def test_save_and_delete_media_success(sample_media_jpg, medias_folder_path):
-    file_path = await save_media(file=sample_media_jpg, path_to_save=medias_folder_path)
+async def test_save_and_delete_media_success(sample_media_jpg, path_to_medias_dir):
+    file_path = await save_media(file=sample_media_jpg, path_to_save=path_to_medias_dir)
     assert file_path.exists()
-    assert file_path.parent == medias_folder_path
+    assert file_path.parent == path_to_medias_dir
     assert file_path.suffix == ".jpg"
 
     await delete_media(file_path=file_path)
     assert not file_path.exists()
 
-    medias_folder_path.rmdir()
-
 
 @pytest.mark.asyncio
-async def test_save_media_not_exist_dir(sample_media_jpg, medias_folder_path):
+async def test_save_media_not_exist_dir(sample_media_jpg, path_to_medias_dir):
+    fake_path_to_medias_dir = path_to_medias_dir / "fake_dir"
+
     with pytest.raises(HTTPException) as exc_info:
-        await save_media(file=sample_media_jpg, path_to_save=medias_folder_path)
+        await save_media(file=sample_media_jpg, path_to_save=fake_path_to_medias_dir)
 
     assert exc_info.value.status_code == 500
 
