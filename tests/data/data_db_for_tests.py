fe878c092de6198e633ee839e9c11d3552a89950
Artyom Kadikov <8961805@gmail.com>
2025-05-02 22:58:21 +0300

    Create file with data using in tests

---
+++ tests/data/data_db_for_tests.py
@@ -0,0 +1,14 @@
+tweet_no_media_valid = {
+    "tweet_data": "Tweet without media!",
+    "tweet_media_ids": [],
+}
+
+tweet_media_valid = {
+    "tweet_data": "Tweet with media!",
+    "tweet_media_ids": [1],
+}
+
+tweet_no_data_invalid = {
+    "tweet_data": "",
+    "tweet_media_ids": [],
+}
