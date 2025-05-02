790516f54135c87d9e8e1561d7b7d1044d938ffc
Artyom Kadikov <8961805@gmail.com>
2025-05-02 23:35:38 +0300

    Create check tweet existing

--- server/api/crud/crud_likes.py
+++ server/api/crud/crud_likes.py
@@ -1,12 +1,20 @@
+from fastapi import HTTPException, status
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from server.core.models import Likes, Users
+from server.core.models import Likes, Tweets, Users
 
 
 async def create_like(
     session: AsyncSession, tweet_id: int, current_user: Users
 ) -> None:
+
+    tweet: Tweets | None = await session.get(Tweets, tweet_id)
+    if not tweet:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Tweet '{tweet_id}' not found!",
+        )
 
     new_like = Likes(tweet_id=tweet_id, user_id=current_user.id)
 
