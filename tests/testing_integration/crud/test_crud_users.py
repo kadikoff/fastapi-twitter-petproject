0f95380ab2b98a7491b57d4c81aab2b344279939
Artyom Kadikov <8961805@gmail.com>
2025-05-02 23:06:12 +0300

    Add docstrings and annotations

--- tests/testing_integration/crud/test_crud_users.py
+++ tests/testing_integration/crud/test_crud_users.py
@@ -3,23 +3,31 @@ from fastapi import HTTPException
 
 from server.api.crud import crud_users
 from server.core.models import Users
+from tests.data.data_db_mock import users_correct
 
 
 @pytest.mark.asyncio
-async def test_get_user_by_api_key_correct(db_session, users_objs):
-    user_obj = users_objs[0]
-    user = await crud_users.get_user_by_api_key(
-        session=db_session, api_key=user_obj.api_key
+async def test_get_user_by_api_key_success(db_session):
+    """Тест успешного получения пользователя по api_key"""
+
+    user_data: dict = users_correct[0]
+
+    user: Users | None = await crud_users.get_user_by_api_key(
+        session=db_session, api_key=user_data["api_key"]
     )
 
     assert user
     assert isinstance(user, Users)
-    assert user.name == user_obj.name
-    assert user.api_key == user_obj.api_key
+    assert user.name == user_data["name"]
+    assert user.api_key == user_data["api_key"]
 
 
 @pytest.mark.asyncio
 async def test_get_user_by_api_key_error(db_session):
+    """Тест обработки ошибки при получении пользователя
+    по api_key с использованием не существующего api_key
+    """
+
     fake_api_key = "fake_api_key"
 
     with pytest.raises(HTTPException) as exc_info:
@@ -32,20 +40,27 @@ async def test_get_user_by_api_key_error(db_session):
 
 
 @pytest.mark.asyncio
-async def test_get_user_by_id_correct(db_session, users_objs):
-    user_obj = users_objs[0]
-    user = await crud_users.get_user_by_id(
-        session=db_session, user_id=user_obj.id
+async def test_get_user_by_id_success(db_session):
+    """Тест успешного получения пользователя по его id"""
+
+    user_data: dict = users_correct[0]
+
+    user: Users | None = await crud_users.get_user_by_id(
+        session=db_session, user_id=user_data["id"]
     )
 
     assert user
     assert isinstance(user, Users)
-    assert user.name == user_obj.name
-    assert user.api_key == user_obj.api_key
+    assert user.name == user_data["name"]
+    assert user.api_key == user_data["api_key"]
 
 
 @pytest.mark.asyncio
 async def test_get_user_by_id_error(db_session):
+    """Тест обработки ошибки при получении
+    пользователя по не существующему id
+    """
+
     fake_user_id = 123456789
 
     with pytest.raises(HTTPException) as exc_info:
@@ -58,23 +73,31 @@ async def test_get_user_by_id_error(db_session):
 
 
 @pytest.mark.asyncio
-async def test_create_follow_correct(db_session, users_objs):
-    user_obj_1 = users_objs[0]
-    user_obj_2 = users_objs[1]
+async def test_create_follow_success(db_session):
+    """Тест успешного создания подписки одного
+    пользователя на другого
+    """
+
+    user_data_1 = Users(**users_correct[0])
+    user_data_2: dict = users_correct[1]
 
     await crud_users.create_follow(
-        session=db_session, current_user=user_obj_1, user_id=user_obj_2.id
+        session=db_session, current_user=user_data_1, user_id=user_data_2["id"]
     )
 
 
 @pytest.mark.asyncio
-async def test_create_follow_error(db_session, users_objs):
-    user_obj = users_objs[0]
+async def test_create_follow_error(db_session):
+    """Тест обработки ошибки при создании
+    подписки одного пользователя на несуществующего
+    """
+
+    user_data = Users(**users_correct[0])
     fake_user_id = 123456789
 
     with pytest.raises(HTTPException) as exc_info:
         await crud_users.create_follow(
-            session=db_session, current_user=user_obj, user_id=fake_user_id
+            session=db_session, current_user=user_data, user_id=fake_user_id
         )
 
     assert exc_info.value.status_code == 404
@@ -82,10 +105,14 @@ async def test_create_follow_error(db_session, users_objs):
 
 
 @pytest.mark.asyncio
-async def test_delete_follow_correct(db_session, users_objs):
-    user_obj_1 = users_objs[0]
-    user_obj_2 = users_objs[1]
+async def test_delete_follow_success(db_session):
+    """Тест успешного удаления подписки одного
+    пользователя на другого
+    """
+
+    user_data_1 = Users(**users_correct[0])
+    user_data_2: dict = users_correct[1]
 
     await crud_users.delete_follow(
-        session=db_session, current_user=user_obj_1, user_id=user_obj_2.id
+        session=db_session, current_user=user_data_1, user_id=user_data_2["id"]
     )
