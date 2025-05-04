cba1e79881fec954c7fbbcaed600007faa480fc6
Artyom Kadikov <8961805@gmail.com>
2025-05-04 21:07:58 +0300

    Del test - no need yet; add docstrings

--- tests/testing_unit/utils/test_media_writer.py
+++ tests/testing_unit/utils/test_media_writer.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import pytest
 from fastapi import HTTPException
 
@@ -11,6 +13,8 @@ from server.utils.media_writer import (
 
 @pytest.mark.asyncio
 async def test_create_medias_directory_success(path_to_medias_dir):
+    """Тест успешного создания директории для хранения медиа"""
+
     await create_medias_directory(path=path_to_medias_dir)
     assert path_to_medias_dir.exists()
 
@@ -19,7 +23,9 @@ async def test_create_medias_directory_success(path_to_medias_dir):
 async def test_save_and_delete_media_success(
     sample_media_jpg, path_to_medias_dir
 ):
-    file_path = await save_media(
+    """Тест успешного сохранения и удаления медиа"""
+
+    file_path: Path | None = await save_media(
         file=sample_media_jpg, path_to_save=path_to_medias_dir
     )
     assert file_path.exists()
@@ -31,20 +37,12 @@ async def test_save_and_delete_media_success(
 
 
 @pytest.mark.asyncio
-async def test_save_media_not_exist_dir(sample_media_jpg, path_to_medias_dir):
-    fake_path_to_medias_dir = path_to_medias_dir / "fake_dir"
-
-    with pytest.raises(HTTPException) as exc_info:
-        await save_media(
-            file=sample_media_jpg, path_to_save=fake_path_to_medias_dir
-        )
-
-    assert exc_info.value.status_code == 500
-
-
-@pytest.mark.asyncio
 async def test_validate_media_not_allowed_ext(sample_media_pdf):
-    file_name = sample_media_pdf.filename
+    """Тест обработки ошибки при создании медиа
+    с неподдерживаемым расширением файла
+    """
+
+    file_name: str = sample_media_pdf.filename
 
     with pytest.raises(HTTPException) as exc_info:
         await validate_media(file_name=file_name)
