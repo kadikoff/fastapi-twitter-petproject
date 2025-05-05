ecb71e73280f18fe2f69b216305ac966157d8df1
Artyom Kadikov <8961805@gmail.com>
2025-05-05 12:35:03 +0300

    Add tags for routers

--- server/api/routes/__init__.py
+++ server/api/routes/__init__.py
@@ -5,6 +5,6 @@ from .routes_tweets import router as tweets_router
 from .routes_medias import router as medias_router
 
 router = APIRouter()
-router.include_router(users_router)
-router.include_router(tweets_router)
-router.include_router(medias_router)
+router.include_router(users_router, tags=["Users"])
+router.include_router(tweets_router, tags=["Tweets"])
+router.include_router(medias_router, tags=["Medias"])
