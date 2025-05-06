64f7ab2947261d43356f64dfc7bde537935bc112
artiom_kadikov <mister-kadikov@yandex.ru>
2025-05-06 20:57:03 +0300

    Change JSONResponse on ORJSONResponse and add logging

--- server/error_handlers.py
+++ server/error_handlers.py
@@ -1,6 +1,10 @@
+import logging
+
 from fastapi import FastAPI, HTTPException, Request, status
 from fastapi.exceptions import RequestValidationError
-from fastapi.responses import JSONResponse
+from fastapi.responses import ORJSONResponse
+
+log = logging.getLogger(__name__)
 
 
 def register_errors_handlers(app: FastAPI):
@@ -9,8 +13,9 @@ def register_errors_handlers(app: FastAPI):
     @app.exception_handler(HTTPException)
     async def custom_error_handler_http(
         request: Request, exc: HTTPException
-    ) -> JSONResponse:
-        return JSONResponse(
+    ) -> ORJSONResponse:
+        log.error("Error: %s", exc.detail)
+        return ORJSONResponse(
             status_code=exc.status_code,
             content={
                 "result": False,
@@ -22,8 +27,9 @@ def register_errors_handlers(app: FastAPI):
     @app.exception_handler(RequestValidationError)
     async def custom_error_handler_validation(
         request: Request, exc: RequestValidationError
-    ) -> JSONResponse:
-        return JSONResponse(
+    ) -> ORJSONResponse:
+        log.error("Error: %s", exc.errors())
+        return ORJSONResponse(
             status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
             content={
                 "result": False,
@@ -35,8 +41,9 @@ def register_errors_handlers(app: FastAPI):
     @app.exception_handler(Exception)
     async def custom_error_handler_sqlalchemy(
         request: Request, exc: Exception
-    ) -> JSONResponse:
-        return JSONResponse(
+    ) -> ORJSONResponse:
+        log.error("Error: %s", str(exc))
+        return ORJSONResponse(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             content={
                 "result": False,
