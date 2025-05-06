ec97807390552cbf164b937a4b1e85edd3a4a4b7
artiom_kadikov <mister-kadikov@yandex.ru>
2025-05-06 20:58:27 +0300

    Del some func and replace on create_app.py

--- server/main.py
+++ server/main.py
@@ -1,46 +1,15 @@
-from contextlib import asynccontextmanager
-from typing import Any, AsyncGenerator
-
 import uvicorn
-from fastapi import FastAPI
 
-from server.api.routes import router
-from server.core.models import Users, db_helper
-from server.error_handlers import register_errors_handlers
+from server.core.config import settings
+from server.create_app import create_app
 
-PORT: int = 8000
-
-
-async def create_mock_data() -> None:
-    user_1 = Users(name="Artyom Kadikov", api_key="test")
-    user_2 = Users(name="Nikita Ivanov", api_key="dev")
-
-    async with db_helper.session_factory() as session:
-        session.add_all([user_1, user_2])
-        await session.commit()
-        await session.close()
-
-
-@asynccontextmanager
-async def lifespan(app: FastAPI) -> AsyncGenerator[None, Any]:
-
-    await create_mock_data()
-
-    yield
-
-    await db_helper.engine.dispose()
-
-
-app = FastAPI(lifespan=lifespan)
-app.include_router(router=router)
-register_errors_handlers(app=app)
-
+app = create_app()
 
 if __name__ == "__main__":
 
     uvicorn.run(
-        host="0.0.0.0",
-        port=8000,
-        app="main:app",
-        reload=True,
+        host=settings.run.host,
+        port=settings.run.port,
+        app=settings.run.app,
+        reload=settings.run.reload,
     )
