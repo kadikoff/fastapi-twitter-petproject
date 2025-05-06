63563efbe3993a4b731e8d6a66e272421191fe0c
artiom_kadikov <mister-kadikov@yandex.ru>
2025-05-06 20:57:41 +0300

    Add configs for Fastapi, uvicorn and logging

--- server/core/config.py
+++ server/core/config.py
@@ -1,6 +1,11 @@
+import logging
 import os
 from pathlib import Path
+from typing import ClassVar, Type
 
+from fastapi.openapi.models import Response
+from fastapi.responses import ORJSONResponse
+from pydantic import BaseModel
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 # Путь до корневой папки проекта
@@ -13,6 +18,42 @@ BASE_MEDIAS_DIR = BASE_PROJECT_DIR / "server/medias"  # /project/server/medias
 
 # Расширения медиа-файлов, допустимые к загрузке вместе с твитами
 MEDIAS_ALLOWED_EXT = {".jpg", ".jpeg", ".png"}
+
+LOG_DEFAULT_FORMAT = (
+    "[%(asctime)s.%(msecs)03d] %(module)10s:%(lineno)-3d "
+    "%(levelname)-7s - %(message)s"
+)
+
+API_DOCS_TITLE = "Twitter-clone API Documentation"
+API_DOCS_DESCRIPTION = (
+    "API-документация для Pet-проекта аналога Twitter. "
+    "В целях тестирования для авторизации используется "
+    "один из двух доступных api_key: test или dev"
+)
+
+
+class FastApiConfig(BaseModel):
+    """Настройки для приложения FastAPI"""
+
+    default_response_class: ClassVar[Type[Response]] = ORJSONResponse
+    docs_title: str = API_DOCS_TITLE
+    docs_description: str = API_DOCS_DESCRIPTION
+
+
+class RunConfig(BaseModel):
+    """Настройки для Uvicorn"""
+
+    host: str = "0.0.0.0"
+    port: int = 8000
+    app: str = "main:app"
+    reload: bool = True
+
+
+class LoggingConfig(BaseModel):
+    """Настройки для логгирования"""
+
+    log_level: int = logging.DEBUG
+    log_format: str = LOG_DEFAULT_FORMAT
 
 
 class DbSettings(BaseSettings):
@@ -28,7 +69,7 @@ class DbSettings(BaseSettings):
     db_host_local: str
     db_port: int
     db_name: str
-    db_echo: bool = True
+    db_echo: bool = False
 
     @property
     def url(self):
@@ -57,7 +98,10 @@ class DbSettings(BaseSettings):
 class Settings(BaseSettings):
     """Корневая конфигурация приложения"""
 
+    api: FastApiConfig = FastApiConfig()
+    run: RunConfig = RunConfig()
     db: DbSettings = DbSettings()
+    logging: LoggingConfig = LoggingConfig()
 
 
 settings = Settings()
