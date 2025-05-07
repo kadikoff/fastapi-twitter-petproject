d09cc9dd54de256a6caf05cb105edb20cf756b89
artiom_kadikov <mister-kadikov@yandex.ru>
2025-05-08 00:15:09 +0300

    Create new func with creating mock data with users for start app

--- server/utils/create_mock_data.py
+++ server/utils/create_mock_data.py
@@ -1,35 +1,46 @@
-import logging
-import socket
+from typing import Sequence
 
-from sqlalchemy import select
+from sqlalchemy import Result, select
 
 from server.core.models import Users, db_helper
+
+from .hashed_api_key import hash_api_key, validate_api_key
 
 
 async def create_mock_data() -> None:
     """Создаёт тестовых пользователей, если их нет в базе"""
 
-    user_1 = Users(name="Ivan Volkov", api_key="test")
-    user_2 = Users(name="Nikita Ivanov", api_key="dev")
+    mock_users = [
+        {"name": "Ivan Volkov", "api_key": "test"},
+        {"name": "Nikita Ivanov", "api_key": "dev"},
+    ]
 
-    try:
-        async with db_helper.session_factory() as session:
-            test_user = await session.execute(
-                select(Users).where(Users.api_key == user_1.api_key)
-            )
-            if not test_user.scalar():
-                session.add(user_1)
+    async with db_helper.session_factory() as session:
+        db_response: Result = await session.execute(select(Users))
+        db_users: Sequence[Users] = db_response.scalars().all()
 
-            dev_user = await session.execute(
-                select(Users).where(Users.api_key == user_2.api_key)
-            )
-            if not dev_user.scalar():
-                session.add(user_2)
+        if db_users:
+            for mock_user in mock_users:
+                user_exists = False
 
-            await session.commit()
-    except socket.gaierror as exc:
-        logging.error(
-            "Ошибка: тестовыe данные пользователей не добавлены! "
-            "Возможно отсутствует подключение к базе данных!\n\n%s",
-            str(exc),
-        )
+                for db_user in db_users:
+                    if validate_api_key(
+                        api_key=mock_user["api_key"],
+                        hashed_api_key=db_user.api_key,
+                    ):
+                        user_exists = True
+                        break
+
+                if not user_exists:
+                    hashed_api_key = hash_api_key(api_key=mock_user["api_key"])
+                    user = Users(
+                        name=mock_user["name"], api_key=hashed_api_key
+                    )
+                    session.add(user)
+                    await session.commit()
+        else:
+            for mock_user in mock_users:
+                hashed_api_key = hash_api_key(api_key=mock_user["api_key"])
+                user = Users(name=mock_user["name"], api_key=hashed_api_key)
+                session.add(user)
+                await session.commit()
